3cdd58a6b14b8c6b2bfd8ae80276b730eab84bf5
FanZejie <61533673+FanZejie@users.noreply.github.com>
2024-06-30 14:26:29 +0800

    responsive burger menu

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,25 +1,51 @@
-import Link from "next/link"
+"use client";
+import Link from "next/link";
 import styles from "./authLinks.module.css";
-export default function AuthLinks(){
-    const status = "notauthenticated"
-    return(
+import { useState } from "react";
+
+
+const AuthLinks = () => {
+  const [open, setOpen] = useState(false);
+
+
+  return (
+    <>
+      {status === "unauthenticated" ? (
+        <Link href="/login" className={styles.link}>
+          Login
+        </Link>
+      ) : (
         <>
-       
-        {status === "notauthenticated" ? (
-            <Link href="/login" className={styles.link}>
-            Login
+          <Link href="/write" className={styles.link}>
+            Write
           </Link>
-        ):(
-            <>
-            <Link href="/write" className={styles.link}>
-              Write
-            </Link>
-            <span className={styles.link} onClick={signOut}>
-              Logout
-            </span>
-          </>
-        )}
+          <span className={styles.link} >
+            Logout
+          </span>
         </>
-        
-    )
-}
+      )}
+      <div className={styles.burger} onClick={() => setOpen(!open)}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+      </div>
+      {open && (
+        <div className={styles.responsiveMenu}>
+          <Link href="/">Homepage</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+          {status === "notauthenticated" ? (
+            <Link href="/login">Login</Link>
+          ) : (
+            <>
+              <Link href="/write">Write</Link>
+              <span className={styles.link}>Logout</span>
+            </>
+          )}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default AuthLinks;
