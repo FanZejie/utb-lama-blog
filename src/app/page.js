db60897d4df80e59881b21188fc77a115dcb6b10
FanZejie <61533673+FanZejie@users.noreply.github.com>
2024-06-30 19:14:32 +0800

    categoryyList

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,12 @@
+import CategoryList from "@/components/categoryList/CategoryList";
+import Featured from "@/components/featured/Featured";
 
 export default function Home() {
   return (
    <>
     <main>
+      <Featured></Featured>
+      <CategoryList></CategoryList>
       <h1>hello</h1>
     </main>
     
