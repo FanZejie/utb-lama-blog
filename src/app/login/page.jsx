3cbfab35930bd1a5fef58fbbb56c49ee8d54ccc4
FanZejie <61533673+FanZejie@users.noreply.github.com>
2024-07-03 23:18:24 +0800

    google login 不好使，但是githublogin成功了，在这卡了好久了，赶紧继续吧

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,11 +1,11 @@
 "use client";
-
+import { signIn, useSession } from "next-auth/react";
 import styles from "./loginPage.module.css";
 import { useRouter } from "next/navigation";
 
 const LoginPage = () => {
- 
-
+  const { status } = useSession();
+  
   const router = useRouter();
 
   if (status === "loading") {
@@ -19,10 +19,11 @@ const LoginPage = () => {
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <div className={styles.socialButton} >
+        <div className={styles.socialButton} onClick={() => signIn("google")}>
           Sign in with Google
         </div>
-        <div className={styles.socialButton}>Sign in with Github</div>
+        <div className={styles.socialButton} onClick={() => signIn("github")}>Sign in with Github</div>
+       
         <div className={styles.socialButton}>Sign in with Facebook</div>
       </div>
     </div>
